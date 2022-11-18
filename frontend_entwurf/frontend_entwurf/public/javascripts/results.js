23cb8f5a75f0da7f09cde7d84cf36c8a74cff725
Jonathan Mader <96208636+JonthMM@users.noreply.github.com>
2022-11-18 16:28:16 +0100

    Delete frontend_entwurf directory

--- frontend_entwurf/frontend_entwurf/public/javascripts/results.js
+++
@@ -1,7 +0,0 @@
-// erstellen einer leaflet Karte mit Europa als Startpunkt und mit OSM als Basiskarte
-var map = L.map("ergebnismap").setView([51.96, 7.6], 12);
-
-L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
