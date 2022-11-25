50f73df34a7837b65d8a5a994dd3a08b528b9a7a
Jonathan Mader <96208636+JonthMM@users.noreply.github.com>
2022-11-25 19:29:22 +0100

    Update app.js

--- frontend_entwurf/app.js
+++ frontend_entwurf/app.js
@@ -3,13 +3,121 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const fs = require("fs");
+const http = require("http");
+const multer = require("multer");
+
+var app = express();
+
+// Uploading data handler for Sentinel 2 png here
+
+// error handler
+const handleError = (err, res) => {
+  res
+    .status(500)
+    .contentType("text/plain")
+    .end("Oops! Something went wrong!");
+};
+
+const upload = multer({
+  dest: "/public"
+  // you might also want to set some limits: https://github.com/expressjs/multer#limits
+});
+
+app.post(
+  "/uploadsentinel",
+  upload.single("file"),
+  (req, res) => {
+    const tempPath = req.file.path;
+    const targetPath = path.join(__dirname, "public/uploads/usersentineldata.tif");
+
+    if (path.extname(req.file.originalname).toLowerCase() === ".tif") {
+      fs.rename(tempPath, targetPath, err => {
+        if (err) return handleError(err, res);
+
+        res
+          .status(200)
+          .contentType("text/plain")
+          .end("File uploaded!");
+      });
+    } else {
+      fs.unlink(tempPath, err => {
+        if (err) return handleError(err, res);
+
+        res
+          .status(403)
+          .contentType("text/plain")
+          .end("Only .png files are allowed!");
+      });
+    }
+  }
+);
+
+app.post(
+  "/uploadtrain",
+  upload.single("file"),
+  (req, res) => {
+    const tempPath = req.file.path;
+    const targetPath = path.join(__dirname, "public/uploads/usertrainingsdata.gpkg");
+
+    if (path.extname(req.file.originalname).toLowerCase() === ".gpkg") {
+      fs.rename(tempPath, targetPath, err => {
+        if (err) return handleError(err, res);
+
+        res
+          .status(200)
+          .contentType("text/plain")
+          .end("File uploaded!");
+      });
+    } else {
+      fs.unlink(tempPath, err => {
+        if (err) return handleError(err, res);
+
+        res
+          .status(403)
+          .contentType("text/plain")
+          .end("Only .png files are allowed!");
+      });
+    }
+  }
+);
+
+app.post(
+  "/uploadmodel",
+  upload.single("file"),
+  (req, res) => {
+    const tempPath = req.file.path;
+    const targetPath = path.join(__dirname, "public/uploads/usertrainedmodel.rds");
+
+    if (path.extname(req.file.originalname).toLowerCase() === ".rds") {
+      fs.rename(tempPath, targetPath, err => {
+        if (err) return handleError(err, res);
+
+        res
+          .status(200)
+          .contentType("text/plain")
+          .end("File uploaded!");
+      });
+    } else {
+      fs.unlink(tempPath, err => {
+        if (err) return handleError(err, res);
+
+        res
+          .status(403)
+          .contentType("text/plain")
+          .end("Only .png files are allowed!");
+      });
+    }
+  }
+);
+
 
 var startRouter = require("./routes/start");
+app.use("/", startRouter);
 var ergebnisseiteRouter = require("./routes/ergebnisseite");
 var anwendungsseiteRouter = require("./routes/anwendungsseite");
 var impressumRouter = require("./routes/impressum");
 
-var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -22,7 +130,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 
-app.use("/", startRouter);
+
 app.use("/ergebnisseite", ergebnisseiteRouter);
 app.use("/anwendungsseite", anwendungsseiteRouter);
 app.use("/impressum", impressumRouter);
