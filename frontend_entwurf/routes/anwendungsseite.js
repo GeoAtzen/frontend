e841f59f3c11170b1260d5ffaa002b169e024b6f
Jonathan Mader <96208636+JonthMM@users.noreply.github.com>
2022-11-29 14:08:25 +0100

    Update anwendungsseite.js

--- frontend_entwurf/routes/anwendungsseite.js
+++ frontend_entwurf/routes/anwendungsseite.js
@@ -1,11 +1,5 @@
 var express = require("express");
 var router = express.Router();
-const MongoClient = require("mongodb").MongoClient;
-
-const url = "mongodb://127.0.0.1:27017"; // connection URL
-const client = new MongoClient(url); // mongodb client
-const dbName = "Lautstärkedaten"; // database name
-const collectionName = "Messungen"; // collection name
 
 /**
  * GET Befehl für die Messungen ansehen Seite
@@ -14,5 +8,14 @@ const collectionName = "Messungen"; // collection name
 router.get("/", function (req, res, next) {
   res.render("anwendungsseite", { title: "Anwendungsseite" });
 });
+
+router.post("/ergebnisseite", function (req, res, next) {
+  // Code zum ausführen des R Skripts
+
+  
+  res.render("ergebnisseite", { title: "Ergebnisseite" })
+  
+});
+
 
 module.exports = router;
